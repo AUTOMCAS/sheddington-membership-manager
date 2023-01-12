af868387522bd1897e241e752847e6b426fc4cdb
Chris <caston88@gmail.com>
2023-01-12 22:45:12 +0000

    Add first passing test for generic 404 error

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const memberRouter = require('./src/routes/member.routes');
 
@@ -10,6 +12,9 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -23,10 +28,17 @@ db.sequelize.sync({ force: true }).then(() => {
   console.log('Drop and re-sync db.');
 });
 
-// simple route
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome!' });
-});
+// A catch-all route for anything the webservice does not define.
+app.get('*', (req, res) =>
+  res.status(404).send({
+    message: 'Hello!',
+  })
+);
+
+// // simple route
+// app.get('/', (req, res) => {
+//   res.json({ message: 'Welcome!' });
+// });
 
 // route setup
 app.use('/members', memberRouter);
--- backend/src/bin/www.js
+++ backend/src/bin/www.js
@@ -1,7 +1,15 @@
+const http = require('http');
 const app = require('../../app');
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
+
+const port = +process.env.PORT || 8080;
+app.set('port', port);
+
+http.createServer(app).listen(port, () => {
+  console.log(`The server is listening on port ${port}`);
 });
