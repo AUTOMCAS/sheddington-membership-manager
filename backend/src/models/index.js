af0ee1d95171015b363dd3ca577c65e0516daa28
Chris <caston88@gmail.com>
2023-01-13 11:24:24 +0000

    Refactor and tidy up comments

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-//const dbConfig = require('../config/db.config');
+
 const env = process.env.NODE_ENV || 'development';
-//const config = require(__dirname + '/../config/config')[env];
+
 const config = require('../config/db.config')[env];
 
 let sequelize = '';
@@ -15,19 +15,6 @@ if (config.use_env_variable) {
     config
   );
 }
-
-// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-//   host: dbConfig.HOST,
-//   dialect: dbConfig.dialect,
-//   operatorsAliases: 0,
-
-//   pool: {
-//     max: dbConfig.pool.max,
-//     min: dbConfig.pool.min,
-//     acquire: dbConfig.pool.acquire,
-//     idle: dbConfig.pool.idle,
-//   },
-// });
 
 const db = {};
 
