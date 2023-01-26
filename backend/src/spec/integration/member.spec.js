b9d6688271b9e48e17a5c5ae72fa2effac234287
Chris <caston88@gmail.com>
2023-01-26 01:23:48 +0000

    Add passing test for POST to members

--- backend/src/spec/integration/member.spec.js
+++ backend/src/spec/integration/member.spec.js
@@ -20,24 +20,18 @@ describe('/members routes', () => {
   });
 
   describe('POST /members', () => {
-    const newMember = {
-      first_name: 'John',
-      second_name: 'Doe',
-      email: 'jd@example.com',
-      telephone: '1234567890',
-      address: '12 example address',
-      gender: 'M',
-      join_date: '01/01/23',
-      renewal_date: '01/01/24',
-    };
-
-    it('responds with a 201 and creates member', (done) => {
-      request(app)
-        .post('/members')
-        .send(newMember)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(201, done);
+    it('the response code is 201', async () => {
+      const response = await request(app).post('/members').send({
+        firstName: 'John',
+        secondName: 'Doe',
+        email: 'jd@example.com',
+        telephone: '1234567890',
+        address: '12 example address',
+        gender: 'M',
+        joinDate: '01/01/23',
+        renewalDate: '01/01/24',
+      });
+      expect(response.statusCode).toBe(200);
     });
   });
   afterAll(async () => {
