4a7f58574d19ef467c206bd85263ab0ccc6bdb7b
Chris <caston88@gmail.com>
2023-01-26 17:21:11 +0000

    Add passing test for creating new user

--- backend/src/spec/integration/member.spec.js
+++ backend/src/spec/integration/member.spec.js
@@ -4,11 +4,17 @@ const app = require('../../../app');
 
 const db = require('../../models');
 
+const Members = db.members;
+
 describe('/members routes', () => {
   const thisDb = db;
   beforeAll(async () => {
     await thisDb.sequelize.sync({ force: true });
   });
+  beforeEach(async () => {
+    await Members.truncate({ cascade: true, restartIdentity: true });
+  });
+
   describe('GET /members', () => {
     it('responds with a 200 and empty member list', (done) => {
       request(app)
@@ -20,7 +26,7 @@ describe('/members routes', () => {
   });
 
   describe('POST /members', () => {
-    it('the response code is 201', async () => {
+    it('the response code is 200 with correct entries', async () => {
       const response = await request(app).post('/members').send({
         firstName: 'John',
         secondName: 'Doe',
@@ -32,6 +38,26 @@ describe('/members routes', () => {
         renewalDate: '01/01/24',
       });
       expect(response.statusCode).toBe(200);
+    });
+    it('creates a new user on db', async () => {
+      const memberExample = {
+        firstName: 'John',
+        secondName: 'Doe',
+        email: 'jd@example.com',
+        telephone: '1234567890',
+        address: '12 example address',
+        gender: 'M',
+        joinDate: '01/01/23',
+        renewalDate: '01/01/24',
+      };
+      await request(app).post('/members').send(memberExample);
+
+      await request(app)
+        .get('/members')
+        .then((res) => {
+          expect(res.body[0].id).toBe(1);
+          expect(res.body[0].first_name).toBe('John');
+        });
     });
   });
   afterAll(async () => {
