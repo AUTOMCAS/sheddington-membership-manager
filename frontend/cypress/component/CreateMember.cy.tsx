4262e4c9c4398fc9d5ee1f7eedffe0e8ba69ce05
Chris <caston88@gmail.com>
2023-02-03 16:52:26 +0000

    Frontend: Add: Test pass render all createMember input fields

--- frontend/cypress/component/CreateMember.cy.tsx
+++ frontend/cypress/component/CreateMember.cy.tsx
@@ -4,4 +4,49 @@ describe('CreateMember.cy.tsx', () => {
   it('renders', () => {
     cy.mount(<CreateMember />);
   });
+  it('Should have input for first name', () => {
+    cy.mount(<CreateMember />);
+    cy.getByData('firstNameInput');
+  });
+  it('Should have input for second name', () => {
+    cy.mount(<CreateMember />);
+    cy.getByData('secondNameInput');
+  });
+  it('Should have input for email', () => {
+    cy.mount(<CreateMember />);
+    cy.getByData('emailInput');
+  });
+  it('Should have input for telephone', () => {
+    cy.mount(<CreateMember />);
+    cy.getByData('telephoneInput');
+  });
+  it('Should have input for address', () => {
+    cy.mount(<CreateMember />);
+    cy.getByData('addressInput');
+  });
+  it('Should have input for gender', () => {
+    cy.mount(<CreateMember />);
+    cy.getByData('genderInput');
+  });
+  it('Should have input for join date', () => {
+    cy.mount(<CreateMember />);
+    cy.getByData('joinDateInput');
+  });
+  it('Should have input for renewal date', () => {
+    cy.mount(<CreateMember />);
+    cy.getByData('renewalDateInput');
+  });
+  it('Should have input for renewal date', () => {
+    cy.mount(<CreateMember />);
+    cy.getByData('renewalDateInput');
+  });
+  xit('Should have a submit button', () => {
+    cy.mount(<CreateMember />);
+    cy.getByData('submit-button');
+  });
+  xit('Should allow a new member to be created', () => {
+    cy.mount(<CreateMember />);
+    cy.getByData('firstNameInput').type('Chris');
+    cy.getByData('submit-button').click();
+  });
 });
--- frontend/src/components/CreateMember/CreateMember.tsx
+++ frontend/src/components/CreateMember/CreateMember.tsx
@@ -9,6 +9,56 @@ const CreateMember: React.FC = () => {
         id="firstName"
         data-test="firstNameInput"
       />
+      <br></br>
+      <input
+        type="input"
+        placeholder="Second Name"
+        id="secondName"
+        data-test="secondNameInput"
+      />
+      <br></br>
+      <input
+        type="input"
+        placeholder="Email"
+        id="email"
+        data-test="emailInput"
+      />
+      <br></br>
+      <input
+        type="input"
+        placeholder="Telephone"
+        id="telephone"
+        data-test="telephoneInput"
+      />
+      <br></br>
+      <input
+        type="input"
+        placeholder="Address"
+        id="address"
+        data-test="addressInput"
+      />
+      <br></br>
+      <input
+        type="input"
+        placeholder="Gender"
+        id="gender"
+        data-test="genderInput"
+      />
+      <br></br>
+      <input
+        type="input"
+        placeholder="Join date"
+        id="joinDate"
+        data-test="joinDateInput"
+      />
+      <br></br>
+      <input
+        type="input"
+        placeholder="Renewal date"
+        id="renewalDate"
+        data-test="renewalDateInput"
+      />
+      <br></br>
     </form>
   );
 };
