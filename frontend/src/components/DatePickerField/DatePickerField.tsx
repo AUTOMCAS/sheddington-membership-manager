fff76f829a30ce53ab86d34451d6fda0dfdaec00
Chris <caston88@gmail.com>
2023-03-30 22:16:59 +0100

    Frontend:DatePickerField: Fix not refeshing error with date formatting

--- frontend/src/components/DatePickerField/DatePickerField.tsx
+++ frontend/src/components/DatePickerField/DatePickerField.tsx
@@ -24,7 +24,7 @@ const DatePickerField: React.FC<DatePickerFieldProps> = ({
         onChange={(val) => {
           helpers.setValue(val);
         }}
-        dateFormat="DD/MM/YYYY"
+        dateFormat="dd/MM/yyyy"
         autoComplete="off"
       />{' '}
       {meta.touched && meta.error ? (
